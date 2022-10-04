f7c9942d3a3640fc0c316afa2899f6573587090c
ProgJeff <112576594+ProgJeff@users.noreply.github.com>
2022-10-04 11:23:08 -0400

    Add files via upload

---
+++ nbjrs2/nbjrs2/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+int jourDepuisDebutAnnee = 0;
+
+const int MoisAnnee = 12;
+int[] joursDansMois = new int[MoisAnnee] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+Console.Write("Entrez le mois courant : ");
+int moisCourant = Convert.ToInt32(Console.In.ReadLine());
+
+Console.Write("Entrez le jour courant : ");
+int jourCourant = Convert.ToInt32(Console.In.ReadLine());
+
+for (int i = moisCourant - 1; i >= 0;)
+{
+    if (i == moisCourant - 1)
+    {
+        jourDepuisDebutAnnee += jourCourant;i--;
+    }
+    else
+    {
+        jourDepuisDebutAnnee += joursDansMois[i];
+        i--;
+
+    }
+}
+Console.Write("Il s'est ecouler " + jourDepuisDebutAnnee + " jours depuis le debut de l'annee");
+Console.ReadKey();
+
